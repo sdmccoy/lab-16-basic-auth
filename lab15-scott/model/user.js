958aa72323707dfd04e0267add8527af0a76d3d6
Scott <sdmccoy@gmail.com>
2017-07-03 15:24:46 -0700

    hash and pw compare function is built

--- lab15-scott/model/user.js
+++ lab15-scott/model/user.js
@@ -13,3 +13,30 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('user', userSchema);
+
+//create methods on the schema to use for encryption logic
+
+//this will take the password inputted on sign up, encode it, and save it as the new hash pw.
+userSchema.methods.passwordHashCreate = function(password){
+  //use hash method from bcrypt, pass in the passord, iterate over 8 times.
+  return bcrypt.hash(password, 8)
+  .then(hash => {
+    this.passwordHash = hash;
+    //returning this to be used in the next function
+    return this;
+  });
+};
+
+//this will compare the password inputted on GET or logins and match it with the encoded passwordHash
+userSchema.methods.passwordHashCompare = function(password){
+  //use the compare method to pass in the pw and compare to the users pwhash value: returns boolean
+  return bcrypt.compare(password, this.passwordHash)
+  .then(isAMatch => {
+    if (isAMatch) {
+      //return the object if it's a match
+      return  this;
+    }
+    //otherwise throw an Error
+    throw new Error('authorization failed. Password did not match');
+  });
+};
