8873e291822016f9d8506c005d20e93c3fbdc33c
Scott <sdmccoy@gmail.com>
2017-07-04 10:25:23 -0700

    built the mock user function to create one user for testing

---
+++ lab15-scott/test/lib/mock-user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const faker = require('faker');
+const User = require('../../model/user.js');
+
+const mockUser = module.exports = {};
+
+mockUser.createOne = () => {
+  let result = {};
+  //set a pw property on result object to use on the hash method
+  result.password = faker.internet.password();
+  //use the constructor to create the new user
+  return new User({
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+  })
+  .passwordHashCreate(result.password)
+  .then(user => {
+    console.log('mock user: ', user);
+    result.user = user;
+    return user.tokenCreate;
+  })
+  .then(token => {
+    console.log('mock token: ', token);
+    result.token = token;
+    return result;
+  });
+};
