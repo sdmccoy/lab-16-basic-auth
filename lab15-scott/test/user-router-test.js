3e9b28aee2db4f1357f99debbe83c37969b00302
Scott <sdmccoy@gmail.com>
2017-07-04 10:50:14 -0700

    POST route test created

--- lab15-scott/test/user-router-test.js
+++ lab15-scott/test/user-router-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const dotenv = require('dotenv').config({path: `../.test.env`});
 const expect = require('expect');
 const superagent = require('superagent');
 const server = require('../lib/server.js');
@@ -10,5 +11,22 @@ const API_URL = process.env.API_URL;
 describe('Testing for user routes', () => {
   before(server.start);
   after(server.stop);
-  afterEach(cleardb());
+  afterEach(clearDB);
+
+  describe('Testing POST route /api/signup', () => {
+    describe('If the post is successful', () => {
+      it('It should return a new user', () => {
+        return superagent.post(`${API_URL}/api/signup`)
+        .send({username: 'dingo', email: 'dogs@example.com', passwordHash: 'secret password'})
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body.username).toEqual('dingo');
+          expect(res.body.email).toEqual('dogs@example.com');
+          expect(res.body.passwordHash).toExist();
+          expect(res.body.tokenSeed).toExist();
+        });
+      });
+    });
+  });
+
 });
